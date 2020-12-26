42d93bd97977457a7a69671ccafae090a659ae04
malenadelaossa <malenadelaossa@hotmail.com>
2020-12-26 14:28:45 -0600

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+
+import { Alert, View, StyleSheet, Text, Linking, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const [cellNumber, setCellNumber] = useState('');
+
+  const [whatsAppMessage, setWhatsAppMessage] = useState();
+
+  const sendMsg = () => {
+  
+    if (cellNumber.length != 8) {
+      Alert.alert('Please Enter Correct WhatsApp Number');
+      return;
+    }
+    // Here we are using 91 which is India Country Code.
+    // You can change country code.
+    let URL = 'whatsapp://send?text=' +  whatsAppMessage + '&phone=506' + cellNumber;
+
+    Linking.openURL(URL)
+      .then((data) => {
+        console.log('WhatsApp Opened');
+      })
+      .catch(() => {
+        Alert.alert('Make sure Whatsapp installed on your device');
+      });
+  };
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <View style={styleSheet.MainContainer}>
+
+        <Text style={styleSheet.text1}>Send WhatsApp Message from React Native App</Text>
+
+        <TextInput
+          value={whatsAppMessage}
+          onChangeText={
+            (whatsAppMessage) => setWhatsAppMessage(whatsAppMessage)
+          }
+          placeholder={'Ingresa tu pedido'}
+          style={styleSheet.textInputStyle}
+        />
+
+        <TextInput
+          value={cellNumber}
+          onChangeText={
+            (cellNumber) => setCellNumber(cellNumber)
+          }
+          placeholder={'Digita tu número de Celular'}
+          keyboardType="numeric"
+          style={styleSheet.textInputStyle}
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={styleSheet.button}
+          onPress={sendMsg}>
+          <Text style={styleSheet.buttonText}> Realizar Pedido </Text>
+        </TouchableOpacity>
+      
     </View>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const styleSheet = StyleSheet.create({
+ 
+  MainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FAFAFA',
+    padding: 10
   },
+
+  text1: {
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+
+  textInputStyle: {
+    height: 42,
+    borderColor: 'blue',
+    borderWidth: 1,
+    width: '100%',
+    paddingHorizontal: 10,
+    marginTop: 20
+  },
+
+  button: {
+    justifyContent: 'center',
+    marginTop: 18,
+    padding: 12,
+    backgroundColor: '#00B8D4',
+  },
+
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  
+ 
 });
